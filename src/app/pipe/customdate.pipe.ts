06cf39ab966a4f24d34ec7a80855eb1b0cd1ed0d
GYEONGHO <rodgh522@softzion.com>
2021-09-16 18:01:10 +0900

    date pipe 생성 	new file:   src/app/pipe/customdate.pipe.spec.ts 	new file:   src/app/pipe/customdate.pipe.ts

---
+++ src/app/pipe/customdate.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customdate'
+})
+export class CustomdatePipe implements PipeTransform {
+
+  transform(origin: Date, ...args: unknown[]): unknown {
+    var result;
+
+    switch (args[0]) {
+      case 'kor':
+        var days = ['일', '월', '화', '수', '목', '금', '토'];
+        result = origin.getFullYear() + '년 ' + (origin.getMonth() + 1) + '월 ' + origin.getDate() + '일(' + days[origin.getDay()] + ')';
+      break;
+    }
+    return result;
+  }
+
+}
