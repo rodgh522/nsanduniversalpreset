4e6d17329b08e581c084c925e932fe83948a2173
GYEONGHO <rodgh522@softzion.com>
2021-09-29 18:01:25 +0900

    서버 배포 작업 	modified:   server.ts 	modified:   src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+    apiKey: "AIzaSyBMAJTMSmnCr7K42mYugJEqWaVCRm1AbDs",
+    authDomain: "fir-adddb.firebaseapp.com",
+    projectId: "fir-adddb",
+    storageBucket: "fir-adddb.appspot.com",
+    messagingSenderId: "316045181441",
+    appId: "1:316045181441:web:49813b9dc5542db4987600",
+    measurementId: "G-14HPXEQ7W3"
+  }
 };
