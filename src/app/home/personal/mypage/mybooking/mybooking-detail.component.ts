25baf0d1e4d7381c9087871410a59e4d9cb0a56c
GYEONGHO <rodgh522@softzion.com>
2021-11-09 15:07:06 +0900

    환불 요청 처리 	modified:   src/app/home/personal/mypage/mybooking/mybooking-detail.component.html 	modified:   src/app/home/personal/mypage/mybooking/mybooking-detail.component.scss 	modified:   src/app/home/personal/mypage/mybooking/mybooking-detail.component.ts 	modified:   src/app/shared/modal/confirm/confirm.component.html

--- src/app/home/personal/mypage/mybooking/mybooking-detail.component.ts
+++ src/app/home/personal/mypage/mybooking/mybooking-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { resvStat } from '@src/app/global/global';
+import { DialogService } from '@src/app/service/dialog.service';
 import { PostApiService } from '@src/app/service/post-api.service';
 
 @Component({
@@ -9,13 +11,16 @@ import { PostApiService } from '@src/app/service/post-api.service';
 })
 export class MybookingDetailComponent implements OnInit {
 
+  today = new Date();
   params;
+  resvStat = resvStat;
   main: any = {};
   subs = [];
   constructor(
     private router: Router,
     private postApi: PostApiService,
-    private activatedRouter: ActivatedRoute
+    private activatedRouter: ActivatedRoute,
+    private dialog: DialogService
   ) { 
     const json = this.activatedRouter.snapshot.params.param;
     if(json) {
@@ -36,6 +41,9 @@ export class MybookingDetailComponent implements OnInit {
     this.postApi.home(param, (res)=> {
       if(res.header.status === 200) {
         const list = res.body.docs;
+        list.forEach(a=> {
+          a.availableCancle = this.checkCancleAvailable(a.CheckinDt, a.ResvStat);
+        });
         this.main = list.filter(a=> a.ResvNo === param.ResvNo)[0];
         this.subs = list.filter(b=> b.ResvNo !== param.ResvNo);
       }
@@ -65,4 +73,55 @@ export class MybookingDetailComponent implements OnInit {
     });
     return total;
   }
+  
+  /**
+   * 예약취소 가능여부 체크
+   * @param utc: DB의 UTC 데이터
+   *       stat: 예약상태
+   * 
+   * Date함수로 초기화하여 해당날짜의 00시와 오늘 00시와 비교,
+   * 예약대기, 예약완료만 취소가능
+   *  */
+  checkCancleAvailable(utc, stat) {
+    const date = new Date(utc);
+    const today = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate());
+    const dDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    const dateAvailable = dDay >= today;
+
+    const statAvailable = stat === 'WAIT' || stat === 'COMP';
+
+    return dateAvailable && statAvailable;
+  }
+  
+  cancelBooking(resv: {[key: string]: any}){
+    const data = {
+      msg: '[' + resv.RoomNm + '] 객실 예약을 취소하시겠습니까?',
+      ok: {
+        msg: '예약취소'
+      },
+      cancel: {
+        msg: '닫기'
+      }
+    };
+    this.dialog.confirm(data).toPromise().then(res=> {
+      if(res) {
+        this.sendRequest(resv);
+      }
+    });
+  }
+
+  sendRequest(resv: any) {
+    const param = {
+      mapcode: 'Reservation.updateBookingStat',
+      ResvNo: resv.ResvNo,
+      ResvStat: 'REQS'
+    };
+
+    this.postApi.movilaUpdate(param, res=>{
+      if(res.header.status === 200) {
+        this.getList();
+      }
+    });
+  }
+
 }
