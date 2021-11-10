8de90bbd730c3db10c2e06c51ffdcc20c6f2995d
GYEONGHO <rodgh522@softzion.com>
2021-11-10 16:28:20 +0900

    toast 추가 	modified:   src/app/home/personal/mypage/info/info.component.tns.html 	modified:   src/app/home/personal/mypage/info/info.component.tns.scss 	modified:   src/app/home/personal/mypage/info/info.component.tns.ts

--- src/app/home/personal/mypage/info/info.component.tns.ts
+++ src/app/home/personal/mypage/info/info.component.tns.ts
@@ -1,34 +1,67 @@
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PostApiService } from '@src/app/service/post-api.service.tns';
 import { SessionService } from '@src/app/service/session.service.tns';
 
 @Component({
   selector: 'ns-info',
   templateUrl: './info.component.tns.html',
-  styleUrls: ['./info.component.tns.scss']
+  styleUrls: ['./info.component.tns.scss'],
 })
 export class InfoComponent implements OnInit, OnDestroy {
 
   user: any = {};
+  toasted = 'hide';
+  toastMsg = '';
   constructor(
     private router: Router,
-    private session: SessionService
+    private session: SessionService,
+    private postApi: PostApiService,
   ) { 
     this.user = this.session.user$.value;
   }
 
   ngOnInit(): void {
-    console.log(this.user);
   }
 
   @HostListener('unloaded')
   ngOnDestroy(){
   }
 
+  doSubmit(){
+    if(this.user['MemNm'] === ''){
+      return;
+    }
+
+    this.sendRequest();
+  }
+
+  sendRequest() {
+    const param = {
+      mapcode: 'MovilaMember.updateMember',
+      ...this.user
+    };
+    this.toasted = 'hide';
+    this.postApi.movilaUpdate(param, (res)=> {
+      if(res.header.status === 200) {
+        this.toast('저장되었습니다.');
+      }else{
+        this.toast('저장되지 않았습니다.');
+      }
+    });
+  }
+  
   signOut(){
     this.session.signOut().then(()=> {
       this.router.navigateByUrl('/main');
     });
   }
 
+  toast(msg: string){
+    if(!msg) {
+      return;
+    }
+    this.toastMsg = msg;
+    this.toasted = 'show';
+  }
 }
